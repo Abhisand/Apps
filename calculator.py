d7e068148f6555d64c3663ee0ee8cba3865985e5
Abhishek <abhisand100@gmail.com>
2018-01-15 20:19:40 +0530

    Simple calculator programe using python

---
+++ calculator.py
@@ -0,0 +1,25 @@
+import  re
+print("Magical Calculator!")
+print("Type 'quit' to Exit")
+previous = 0
+run = True
+
+def show():
+   global  run,previous
+   if previous == 0:
+      equation=input("Enter Equation:")
+   else:
+      equation=input(str(previous))
+   if equation == "quit":
+      run = False
+   else:
+      equation=re.sub('[aA-zZ,"£$(),/?.,@"]',"",equation)
+      if previous == 0:
+         previous=eval(equation)
+      else:
+         previous=eval(str(previous) + equation)
+
+
+while run:
+   show()
+
